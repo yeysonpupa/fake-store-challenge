322ad5c0f373b278da6e06023d0beee178ac7525
yeysonpupa <yeysonpupa@unisabana.edu.co>
2023-10-30 18:47:40 -0500

    Realiza pruebas unitarias

---
+++ src/_tests_/app.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import App from '../App';
+
+test('La aplicación renderiza correctamente la navbar', () => {
+    render(<App />);
+
+    //Obtener la navbar por classname
+    expect(screen.getByRole('navigation')).toHaveClass('navbar')
+});
+
+test('La aplicación renderiza correctamente los textos', () => {
+    render(<App />);
+
+    //Verifica los textos
+    const Titulo = screen.getByText('Fake Store');
+    expect(Titulo).toBeInTheDocument();
+
+    const opcion1 = screen.getByText('Todos');
+    expect(opcion1).toBeInTheDocument();
+
+    const opcion2 = screen.getByText('Ropa de hombre');
+    expect(opcion2).toBeInTheDocument();
+
+    const opcion3 = screen.getByText('Joyería');
+    expect(opcion3).toBeInTheDocument();
+
+    const opcion4 = screen.getByText('Electrónica');
+    expect(opcion4).toBeInTheDocument();
+
+    const opcion5 = screen.getByText('Ropa de mujer');
+    expect(opcion5).toBeInTheDocument();
+
+    const productos = screen.getByText('Se están mostrando 0 de 0 productos');
+    expect(productos).toBeInTheDocument();
+
+    const autor = screen.getByText('Yeyson Pulido');
+    expect(autor).toBeInTheDocument();
+});
---
+++ src/_tests_/navbar.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '@testing-library/jest-dom'
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import Navbar from '../components/navbar';
+
+test('Navbar renderiza las rutas', () => {
+  render(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>
+  );
+
+  //Verificar que existen las rutas 
+  const homeLink = screen.getByText('Home');
+  expect(homeLink).toHaveAttribute('href', '/');
+
+  const cartLink = screen.getByText('Carrito 🛒');
+  expect(cartLink).toHaveAttribute('href', '/cart');
+});
